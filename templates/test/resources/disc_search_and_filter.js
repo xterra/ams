cc3ff7568b5f0a705c4ff9442b2f96b9141d69d6
Александр Подшивалов <ksndr@MacBook-Pro-Aleksandr.local>
2020-04-01 21:09:39 +0300

    Added mutually exclusive search and filter for disciplines. Fixed some bugs for better compatibility with old version on prod.

---
+++ templates/test/resources/disc_search_and_filter.js
@@ -0,0 +1,53 @@
+let searchAndFilter = function() {
+  let filterForm = document.getElementById("filter"),
+      inputForm = document.getElementById("search"),
+      disciplines = document.querySelectorAll('.col-sm-6');
+
+  filterForm.addEventListener("change", searchAndFilterHandler);
+  inputForm.addEventListener("input", searchAndFilterHandler);
+
+  function searchAndFilterHandler(){
+    let typesForFilter = getValuesFromCheckboxes('types'),
+        groupsForFilter = getValuesFromCheckboxes('groups'),
+        coursesForFilter = getValuesFromCheckboxes('courses');
+
+    let splitSearchPhrases = inputForm.value.toLowerCase().split(' ');
+
+    disciplines.forEach(currentDisc => {
+      if(typesForFilter.length == 0 && groupsForFilter.length == 0 && coursesForFilter.length == 0 && splitSearchPhrases.length == 0){
+        currentDisc.style.display = '';
+      } else {
+        let typeFilterResult = checkCoincidence(typesForFilter);
+        let groupFilterResult = checkCoincidence(groupsForFilter);
+        let courseFilterResult = checkCoincidence(coursesForFilter);
+        let searchResult = splitSearchPhrases.length == 0 ? true : splitSearchPhrases.reduce((total, currentPhrase) => {
+          if(currentDisc.textContent.toLowerCase().indexOf(currentPhrase) > -1){
+            return total = (total && 1);
+          } else {
+            return total = 0;
+          }
+        }, 1);
+
+        if(typeFilterResult && groupFilterResult && courseFilterResult && searchResult){
+          currentDisc.style.display = '';
+        }else{
+          currentDisc.style.display = 'none';
+        }
+      }
+      function checkCoincidence(phrasesThatCheck){
+        return phrasesThatCheck.length == 0 ? true : phrasesThatCheck.reduce((total, currentPhrase) => {
+          if(currentDisc.textContent.toLowerCase().indexOf(currentPhrase) > -1){
+            return total = (total || 1);
+          } else {
+            return total = (total || 0);
+          }
+        }, 0);
+      }
+    });
+  }
+  function getValuesFromCheckboxes(nameCheckbox){
+    let selectedCheckboxes = document.querySelectorAll(`input[name=${nameCheckbox}]:checked`);
+    return [].map.call(selectedCheckboxes, (currentCheckbox) => {return currentCheckbox.value.toLowerCase()});
+  }
+}
+searchAndFilter();
