b0e74552ff23c0e9063c6e9fadb126ee0f36c5e9
Alex Podshivalov <ksndr.pdsh@gmail.com>
2020-09-29 19:44:56 +0300

    Allowing to download ZIP starts from 2 selected files

--- templates/test/resources/checkbox_hide_button.js
+++ templates/test/resources/checkbox_hide_button.js
@@ -2,7 +2,7 @@ function showOrHideButton(checkboxName){
   let submitBtn = document.getElementById('submit');
   let checkedValues = document.querySelectorAll(`input[name=${checkboxName}]:checked`);
   console.log(checkedValues);
-  if(checkedValues.length == 0){
+  if(checkedValues.length <= 1){
     submitBtn.disabled = true;
   } else {
     submitBtn.disabled = false;
