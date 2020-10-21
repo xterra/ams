cc051c3b2970887cafbdb3e2bc8230f9b83fadf0
Alex Podshivalov <ksndr.pdsh@gmail.com>
2020-10-21 20:24:43 +0300

    Changed query logic for view last news

--- templates/test/processors/home.js
+++ templates/test/processors/home.js
@@ -4,9 +4,7 @@ const router = require('../../../router'),
 module.exports = {
     path: new RegExp("^\/$"),
     processor: function (request, response, callback, sessionContext, sessionToken, db) {
-        let dateNow = new Date();
-        let dateMonthAgo = dateNow.setMonth(dateNow.getMonth() - 1);
-        db.collection("news").find({dateUpdate: {$gte: new Date(dateMonthAgo)}}).sort({dateUpdate: -1}).limit(3).toArray(function(err, result){
+        db.collection("news").find().sort({dateUpdate: -1}).limit(3).toArray(function(err, result){
           if(err){
             callback();
             return router.bleed(500, null, response)
