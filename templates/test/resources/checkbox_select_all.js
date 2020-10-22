3b4f1dd9c57d5e0352c812f3dcfa516b04d6d6ed
Alex Podshivalov <ksndr.pdsh@gmail.com>
2020-10-22 21:19:58 +0300

    Fixed bug of selecting files to downloading by the archive

--- templates/test/resources/checkbox_select_all.js
+++ templates/test/resources/checkbox_select_all.js
@@ -1,3 +1,36 @@
+document.addEventListener('change', () => {
+  const chk = event.target;
+
+  if(chk.tagName == 'INPUT'){
+    if(chk.name == 'files'){
+      let fileCheckboxes = document.querySelectorAll(`input[name=files]`);
+      let countSelected = 0;
+      let allSelected = true;
+      for (let elem of fileCheckboxes) {
+        if(elem.checked) countSelected++;
+        allSelected =  allSelected && elem.checked;
+        if(countSelected > 1 && !allSelected) break;
+      }
+      showOrHideSubmit(countSelected);
+      changeAllFilesCheckbox(allSelected);
+    }
+    if(chk.name == 'allFiles'){
+      selectAllCheckboxes('files');
+    }
+  }
+})
+
+function showOrHideSubmit(countSelected){
+  let submitBtn = document.getElementById('submit');
+  if(countSelected <= 1) return submitBtn.disabled = true;
+  submitBtn.disabled = false;
+}
+
+function changeAllFilesCheckbox(allSelected){
+  let allFilesCheckbox = document.getElementById('allFiles');
+  allFilesCheckbox.checked = allSelected;
+}
+
 function selectAllCheckboxes(checkboxName){
   let allFilesCheckbox = document.getElementById('allFiles');
   let checkboxList = document.querySelectorAll(`input[name=${checkboxName}]`);
