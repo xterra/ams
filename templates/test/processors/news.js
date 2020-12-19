d6b6ebcfb135a845f48ccea2b3b386b33b296ea6
Alex Podshivalov <ksndr.pdsh@gmail.com>
2020-12-19 14:00:02 +0300

    Changed sorting news and values cache time

--- templates/test/processors/news.js
+++ templates/test/processors/news.js
@@ -4,7 +4,6 @@ const bleed = require('../../../router').bleed,
 module.exports = {
   path: new RegExp("^\/news\/$"),
   processor: function(request, response, callback, sessionContext, sessionToken, db) {
-    console.log(sessionContext);
     db.collection("news").aggregate([
        {
          $lookup:
@@ -22,22 +21,21 @@ module.exports = {
              as: "authorInfo"
            }
         },
-    ]).sort({name: 1}).toArray(function(err, result){
+    ]).sort({dateUpdate: -1}).toArray(function(err, result){
       if(err) {
         callback();
         return bleed(500, null, response, err);
       }
       const news = result;
-      console.log(JSON.stringify(news[0].authorInfo));
-      for (let pieceOfNews in news){
-        news[`${pieceOfNews}`].formatedDate = beautyDate(news[pieceOfNews].dateUpdate);
+      for (let pieceOfNews of news){
+        pieceOfNews.formatedDate = beautyDate(pieceOfNews.dateUpdate);
       }
       if(sessionToken == null || sessionContext == undefined || sessionContext == null){
       callback({
         title: "Новости",
         news: news,
         userInfo: null
-      }, "news", 5, 5);
+      }, "news", 0, 5);
     } else{
         db.collection("users").findOne({_id: sessionContext.id}, {username: 1, securityRole: 1}, function(err, result){
           if(err){
@@ -49,7 +47,7 @@ module.exports = {
             title: "Новости",
             news: news,
             userInfo: user
-          }, "news", 5, 5);
+          }, "news", 0, 5);
         });
       }
     });
