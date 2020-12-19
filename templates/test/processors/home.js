9d36b28f11cfcc4794e5884605b6397d963e5d54
Alex Podshivalov <ksndr.pdsh@gmail.com>
2020-12-19 13:58:48 +0300

    Fixed auth checking and values cache times

--- templates/test/processors/home.js
+++ templates/test/processors/home.js
@@ -2,35 +2,28 @@ const router = require('../../../router'),
       beautyDate = require('../../../beautyDate');
 
 module.exports = {
-    path: new RegExp("^\/$"),
-    processor: function (request, response, callback, sessionContext, sessionToken, db) {
-        db.collection("news").find().sort({dateUpdate: -1}).limit(3).toArray(function(err, result){
-          if(err){
-            callback();
-            return router.bleed(500, null, response)
-          }
-          let news = result;
-          console.log(news);
-          for (let pieceOfNews in news){
-            console.log(JSON.stringify(news[pieceOfNews]));
-            news[`${pieceOfNews}`].formatedDate = beautyDate(news[pieceOfNews].dateUpdate);
-          }
-          console.log(JSON.stringify(news));
-          if(sessionToken == null || sessionContext == undefined || sessionContext == null){
-            callback({
-                title: "Главная",
-                news: news,
-                userAthorized: false
-            }, "home", 5, 5);
-          }else{
-            callback({
-                title: "Главная",
-                news: news,
-                userAthorized: true
-            }, "home", 5, 5);
-          }
+  path: new RegExp('^\/$'),
+  processor: function (request, response, callback, sessionContext, sessionToken, db) {
+    db.collection('news').find().sort({dateUpdate: -1}).limit(3).toArray( (err, result) => {
+      if(err){
+        callback();
+        return router.bleed(500, null, response)
+      }
+      let news = result;
+      for (let pieceOfNews of news){
+        pieceOfNews.formatedDate = beautyDate(pieceOfNews.dateUpdate);
+      }
+      callback({
+        title: 'Главная',
+        news: news,
+        userAthorized: isUserAuthed(sessionContext, sessionToken)
+      }, 'home', 0, 0);
+    });
+  }
+}
 
-        });
-
-    }
+function isUserAuthed(sessionContext, sessionToken) {
+  return (typeof sessionToken === 'string' &&
+    sessionContext instanceof Object &&
+    sessionContext['id'] !== undefined);
 }
