def009a843a2c886b2d3bbcbe60827626044d91a
Alex Podshivalov <ksndr.pdsh@gmail.com>
2020-12-31 07:45:44 +0300

    Refactored processor news_delete

--- templates/test/processors/news_delete.js
+++ templates/test/processors/news_delete.js
@@ -2,56 +2,119 @@ const router = require('../../../router'),
       ObjectID = require('mongodb').ObjectID;
 
 module.exports = {
-  path: new RegExp("^\/news\/delete\/[^\/]+\/$"),
-  processor: function(request, response, callback, sessionContext, sessionToken, db){
-    if(sessionToken == null || sessionContext == undefined || sessionContext == null){
-      callback();
-      return router.bleed(301, "/login/", response);
-    }
-    const requsetedURL = decodeURI(request.url);
-    const delimeteredURL = requsetedURL.split("/");
-    const newsUrl = delimeteredURL[delimeteredURL.length - 2];
-    db.collection("users").findOne({_id: sessionContext.id},{username: 1, securityRole: 1}, function(err, result){
-      if(err){
-        callback();
-        return  router.bleed(500, null, response);
-      }
-      if(result == null){
-        callback();
-        return router.bleed(301, "/login/", response);
-      }
+  path: new RegExp('^\/news\/delete\/[^\/]+\/$'),
+  processor(request, response, callback, sessionContext, sessionToken, db) {
+    const userAuthed = isUserAuthed(sessionContext, sessionToken);
+    if(!userAuthed) return redirectToLoginPage(response, callback);
+
+    getRoleForAuthedUser(sessionContext.id, db, (err, result) => {
+      if(err) return redirectTo500Page(response, err, callback);
+      if(result == null) return redirectToLoginPage(response, callback);
+
       const userInfo = result;
-      db.collection("news").findOne({url: newsUrl},{url: 1, title: 1, author: 1}, function(err, result){
-        if(err){
-          callback();
-          return router.bleed(500, null, response, err);
+
+      const userAdminOrTeacher = isUserAdminOrTeacher(userInfo);
+      if(!userAdminOrTeacher) {
+        const err = new Error('User role not admin or teacher');
+        return redirectWithErrorCode(response, 403, err, callback);
+      }
+
+      const newsUrl = getNewsUrlFromRequest(request.url);
+      findTitleAndAuthorForNews(newsUrl, db, (err, result) => {
+        if(err) return redirectTo500Page(response, callback);
+        if(result == null) return redirectTo404Page(response, request.url, callback);
+
+        let newsDetail = result;
+        const deletePermission = checkDeletePermissions(userInfo, newsDetail.author);
+        if(!deletePermission) {
+          const err = new Error('For this user permissions not enough');
+          return redirectWithErrorCode(response, 403, err, callback);
         }
-        if(result == null){
-          callback();
-          return router.bleed(404, requsetedURL, response);
-        }
-        let news_detail = result;
-        if(userInfo.securityRole.length == 0 || (!userInfo.securityRole.includes("superadmin") && userInfo._id.toString() !== news_detail.author)){
-          callback();
-          return router.bleed(403, null, response);
-        }
-        if(request.method == "POST"){
-          db.collection("news").deleteOne({_id: new ObjectID(news_detail._id)}, function(err){
-            if(err){
-              callback();
-              return router.bleed(500, null, response, err);
-            }
+
+        if(request.method == 'POST') {
+          deleteNews(newsDetail._id, db, err => {
+            if(err) return redirectTo500Page(response, err, callback);
             callback();
-            console.log(`News "${newsUrl}" deleted!`);
-            return router.bleed(301, "/news/", response);
+            console.log(`News '${newsUrl}' deleted!`);
+            return router.bleed(301, '/news/', response);
           });
-        } else{
+        } else {
           return callback({
-            title: "Удаление новости",
-            news_detail: news_detail
-          }, "news_delete", 0, 0);
+            title: 'Удаление новости',
+            news_detail: newsDetail
+          }, 'news_delete', 0, 0);
         }
       });
     });
   }
+}
+function isUserAuthed(sessionContext, sessionToken) {
+  return (typeof sessionToken === 'string' &&
+    sessionContext instanceof Object &&
+    sessionContext['id'] !== undefined);
+}
+
+function redirectToLoginPage(response, callback) {
+  callback();
+  return router.bleed(301, '/login/', response);
+}
+
+function getNewsUrlFromRequest(clientUrl){
+  let decodedURL = decodeURI(clientUrl);
+  let delimeteredURL = decodedURL.split('/');
+  return delimeteredURL[delimeteredURL.length - 2];
+}
+
+function getRoleForAuthedUser(userID, db, callback) {
+  db.collection('users').findOne(
+    { _id: new ObjectID(userID) },
+    { _id: 1, securityRole: 1},
+    callback);
+}
+
+function redirectTo500Page(response, err, callback) {
+  callback();
+  return router.bleed(500, null, response, err);
+}
+
+function isUserAdminOrTeacher(userInfo) {
+  const userRoles = userInfo.securityRole;
+  if(userRoles.length !== 0) {
+    return userRoles.includes('superadmin') ||
+    userRoles.includes('admin') ||
+    userRoles.includes('teacher');
+  }
+  return false;
+}
+
+function redirectWithErrorCode(response, code, err, callback) {
+  callback();
+  return router.bleed(code, null, response, err);
+}
+
+function findTitleAndAuthorForNews(newsUrl, db, callback) {
+  db.collection('news').findOne(
+    { url: newsUrl },
+    { url: 1, title: 1, author: 1 },
+    callback);
+}
+
+function redirectTo404Page(response, requestedURL, callback){
+  callback();
+  return router.bleed(404, requestedURL, response);
+}
+
+function checkDeletePermissions(userInfo, newsAuthorID) {
+  const userRoles = userInfo.securityRole;
+  const userID = userInfo._id.toString();
+
+  return userRoles.includes('superadmin') ||
+    userRoles.includes('admin') ||
+    (userID == newsAuthorID);
+}
+
+function deleteNews(newsID, db, callback) {
+  db.collection('news').deleteOne(
+    { _id: new ObjectID(newsID) },
+    callback);
 }
