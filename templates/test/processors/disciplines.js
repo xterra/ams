983195b4cfbc7dcf38c4c9b02f76cb5908c20a1c
Alex Podshivalov <ksndr.pdsh@gmail.com>
2021-02-25 18:38:40 +0300

    Deleted console.log and tabs, small refactoring

--- templates/test/processors/disciplines.js
+++ templates/test/processors/disciplines.js
@@ -1,11 +1,13 @@
 const qs = require('querystring'),
-      router = require('../../../router'),
-      security = require('../../../security');
+      router = require('../../../router');
 
 module.exports = {
   path: new RegExp('^\/disciplines\/$'),
-  processor: function(request, response, callback, sessionContext, sessionToken, db){
-    console.log(JSON.stringify(sessionContext));
+  processor(request, response, callback, sessionContext, sessionToken, db){
+    if( sessionContext == undefined || sessionContext == null || sessionContext['id'] !== undefined) {
+      callback();
+      return router.bleed(301, '/login/', response);
+    }
     db.collection('disciplines').aggregate([
       {
         $lookup:
@@ -39,69 +41,64 @@ module.exports = {
               as: 'editorsInfo'
             }
          },
-    ]).sort({name: 1}).toArray(function(err, result){
-      if(err){
+    ]).sort({name: 1}).toArray((err, result) => {
+      if(err) {
         callback();
         router.bleed(500, null, response, err);
       }
-      console.log(JSON.stringify(result[0].editorsInfo));
-      console.log(JSON.stringify(result[3].editorsInfo));
       const disciplines = result;
-      if( sessionContext !== undefined && sessionContext !== null && 'id' in sessionContext){
-        db.collection('users').findOne({_id: sessionContext.id}, {securityRole : 1, username : 1, group: 1}, function(err, result){
-          if(err){
-            callback();
-            return router.bleed(500, null, response, err);
-          }
-          let userInfo = result;
-          console.log(userInfo.group);
-          if(userInfo.securityRole.length !== 0 && userInfo.securityRole.includes('teacher')){
-            let teacherDisciplines = [],
-                otherDisciplines = [];
-            for(let disc in disciplines){
-              if(disciplines[disc].editors !== undefined && disciplines[disc].editors.includes(userInfo._id.toString())){
-                teacherDisciplines.push(disciplines[disc]);
-              } else{
-                otherDisciplines.push(disciplines[disc]);
-              }
+      db.collection('users').findOne(
+        { _id: sessionContext.id },
+        { securityRole : 1, username : 1, group: 1 },
+      (err, result) => {
+        if(err) {
+          callback();
+          return router.bleed(500, null, response, err);
+        }
+        let userInfo = result;
+        if(userInfo.securityRole.length !== 0 && userInfo.securityRole.includes('teacher')) {
+          let teacherDisciplines = [],
+              otherDisciplines = [];
+          for(let disc of disciplines) {
+            if(disc.editors !== undefined && disc.editors.includes(userInfo._id.toString())) {
+              teacherDisciplines.push(disc);
+            } else {
+              otherDisciplines.push(disc);
             }
-            return callback({
-              title: 'Дисциплины',
-              urlDiscDetail: '/disciplines/',
-              teacherDisciplines: teacherDisciplines,
-              otherDisciplines: otherDisciplines,
-              userInfo: userInfo
-            }, 'disciplines', 0, 0);
-          } else if(userInfo.securityRole.length !== 0 && userInfo.securityRole.includes('student') && userInfo.group !== undefined){
-            let studentDisciplines = [],
-                otherDisciplines = [];
-            for(let disc in disciplines){
-              if(disciplines[disc].groups !== undefined && disciplines[disc].groups.includes(userInfo.group.toString())) {
-                studentDisciplines.push(disciplines[disc]);
-              }else {
-                otherDisciplines.push(disciplines[disc]);
-              }
-            }
-            return callback({
-              title: 'Дисциплины',
-              urlDiscDetail: '/disciplines/',
-              studentDisciplines: studentDisciplines,
-              otherDisciplines: otherDisciplines,
-              userInfo: userInfo
-            }, 'disciplines', 0, 0);
-          } else{
-            return callback({
-              title: 'Дисциплины',
-              urlDiscDetail: '/disciplines/',
-              disciplines: disciplines,
-              userInfo: userInfo
-            }, 'disciplines', 0, 0);
           }
-        });
-      } else {
-        callback();
-        return router.bleed(301, '/login/', response);
-      }
+          return callback({
+            title: 'Дисциплины',
+            urlDiscDetail: '/disciplines/',
+            teacherDisciplines: teacherDisciplines,
+            otherDisciplines: otherDisciplines,
+            userInfo: userInfo
+          }, 'disciplines', 0, 0);
+        } else if(userInfo.securityRole.length !== 0 && userInfo.securityRole.includes('student') && userInfo.group !== undefined) {
+          let studentDisciplines = [],
+              otherDisciplines = [];
+          for(let disc in disciplines){
+            if(disciplines[disc].groups !== undefined && disciplines[disc].groups.includes(userInfo.group.toString())) {
+              studentDisciplines.push(disciplines[disc]);
+            } else {
+              otherDisciplines.push(disciplines[disc]);
+            }
+          }
+          return callback({
+            title: 'Дисциплины',
+            urlDiscDetail: '/disciplines/',
+            studentDisciplines: studentDisciplines,
+            otherDisciplines: otherDisciplines,
+            userInfo: userInfo
+          }, 'disciplines', 0, 0);
+        } else {
+          return callback({
+            title: 'Дисциплины',
+            urlDiscDetail: '/disciplines/',
+            disciplines: disciplines,
+            userInfo: userInfo
+          }, 'disciplines', 0, 0);
+        }
+      });
     });
   }
 }
