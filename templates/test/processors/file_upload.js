64a34e81f0df96de55ddc34d123f0fe152e57e18
Alex Podshivalov <ksndr.pdsh@gmail.com>
2021-06-16 18:48:54 +0300

    Delete POST logic from if statement

--- templates/test/processors/file_upload.js
+++ templates/test/processors/file_upload.js
@@ -25,7 +25,8 @@ module.exports = {
 
       dbMethods.findDisciplineByAllias(disciplineAllias, db, (err, result) => {
         if (err) return bw.redirectTo500Page(response, err, callback);
-        if (result === null) return bw.redirectTo404Page(response, request.url, callback);
+        if (result === null)
+            return bw.redirectTo404Page(response, request.url, callback);
         const discipline = result;
 
         const teacherEditor = check.isTeacherDiscEditor(userInfo, discipline);
@@ -34,52 +35,7 @@ module.exports = {
           return bw.redirectWithErrorCode(response, 403, err, callback);
         }
 
-        if (request.method === 'POST') {
-
-          return router.downloadClientPostData(request, (err, data) => {
-            if (err) return bw.redirectTo500Page(response, err, callback);
-
-            try {
-              const postData = qs.parse(data);
-              const fileID = postData.fileID;
-
-              funcs.moveFileFromTmpStorage(fileID, err => {
-                if (err) {
-                  console.log(`Something wrong with move ${fileID}: ${err}`);
-                  return callback({
-                    title: 'Загрузка файла',
-                    discipline,
-                    userInfo,
-                    errorMessage: 'Проблема с загрузкой файла. Попробуйте снова.',
-                    message: ''
-                  }, 'file_upload', 0, 0);
-                }
-                console.log(`File ${fileID} moved to - /data/private/`);
-
-                dbMethods.addFileInfoToDB(postData, userInfo, db, err => {
-                  if (err) return bw.redirectTo500Page(response, err, callback);
-                  console.log(`Info for file ${fileID} added to DB`);
-
-                  dbMethods.addFileIdToDiscipline(disciplineAllias, fileID, db, err => {
-                    if (err) return bw.redirectTo500Page(response, err, callback);
-                    console.log(`File ${fileID} added to discipline ${disciplineAllias}`);
-
-                    return callback({
-                      title: 'Загрузка файла',
-                      discipline,
-                      userInfo,
-                      errorMessage: '',
-                      message: `Файл загружен. Добавлен к '${discipline.name}'`,
-                    }, 'file_upload', 0, 0);
-                  }); //addFileIdToDiscipline
-                }); //addFileInfoToDB
-              }); //moveFileFromTmpStorage
-            } catch (err) {
-              console.log(err);
-              return bw.redirectTo500Page(response, err, callback);
-            }
-          }); // downloadClientPostData
-        } else {
+        if (request.method !== 'POST') {
           return callback({
             title: 'Загрузка файла',
             discipline,
@@ -88,6 +44,50 @@ module.exports = {
             message: ''
           }, 'file_upload', 0, 0);
         }
+
+        return router.downloadClientPostData(request, (err, data) => {
+          if (err) return bw.redirectTo500Page(response, err, callback);
+
+          try {
+            const postData = qs.parse(data);
+            const fileID = postData.fileID;
+
+            funcs.moveFileFromTmpStorage(fileID, err => {
+              if (err) {
+                console.log(`Something wrong with move ${fileID}: ${err}`);
+                return callback({
+                  title: 'Загрузка файла',
+                  discipline,
+                  userInfo,
+                  errorMessage: 'Проблема с загрузкой файла. Попробуйте снова.',
+                  message: ''
+                }, 'file_upload', 0, 0);
+              }
+              console.log(`File ${fileID} moved to - /data/private/`);
+
+              dbMethods.addFileInfoToDB(postData, userInfo, db, err => {
+                if (err) return bw.redirectTo500Page(response, err, callback);
+                console.log(`Info for file ${fileID} added to DB`);
+
+                dbMethods.addFileIdToDiscipline(disciplineAllias, fileID, db, err => {
+                  if (err) return bw.redirectTo500Page(response, err, callback);
+                  console.log(`File ${fileID} added to discipline ${disciplineAllias}`);
+
+                  return callback({
+                    title: 'Загрузка файла',
+                    discipline,
+                    userInfo,
+                    errorMessage: '',
+                    message: `Файл загружен. Добавлен к '${discipline.name}'`,
+                  }, 'file_upload', 0, 0);
+                }); //addFileIdToDiscipline
+              }); //addFileInfoToDB
+            }); //moveFileFromTmpStorage
+          } catch (err) {
+            console.log(err);
+            return bw.redirectTo500Page(response, err, callback);
+          }
+        }); // downloadClientPostData
       }); //findDisciplineByAllias
     }); //getRoleForAuthedUser
   }
