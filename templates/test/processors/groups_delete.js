c289d857f4d9674af68d0d3bb70dbe046d04224b
Alex Podshivalov <ksndr.pdsh@gmail.com>
2021-06-19 11:22:56 +0300

    Rewrited with funcs and dbMethods for group

--- templates/test/processors/groups_delete.js
+++ templates/test/processors/groups_delete.js
@@ -1,63 +1,55 @@
-const router = require('../../../router'),
-  ObjectID = require('mongodb').ObjectID;
+const bw = require('./common/bleed_wrapper.js'),
+  check = require('./common/permission_check.js'),
+  funcs = require('./groups/funcs.js'),
+  dbMethods = require('./groups/dbMethods.js');
 
 module.exports = {
   path: new RegExp('^/groups/delete/[^/]+/$'),
   processor(request, response, callback, sessionContext, sessionToken, db) {
-    if (sessionToken == null || sessionContext == undefined || sessionContext == null) {
-      callback();
-      return router.bleed(301, '/login/', response);
-    }
-    const requestedUrl = decodeURI(request.url);
-    const delimeteredUrl = requestedUrl.split('/');
-    const groupURL = delimeteredUrl[delimeteredUrl.length - 2];
-    db.collection('users').findOne({ _id: sessionContext.id }, { username: 1, securityRole: 1 }, (err, result) => {
-      if (err) {
-        callback();
-        return router.bleed(500, null, response, err);
-      }
-      if (result == null) {
-        callback();
-        return router.bleed(301, '/login/', response);
-      }
+
+    const userAuthed = check.isUserAuthed(sessionContext, sessionToken);
+    if (!userAuthed) return bw.redirectToLoginPage(response, callback);
+
+    dbMethods.getRoleForAuthedUser(sessionContext.id, db, (err, result) => {
+      if (err) return bw.redirectTo500Page(response, err, callback);
       const userInfo = result;
-      if (userInfo.securityRole.length == 0 || ((!userInfo.securityRole.includes('superadmin') && !userInfo.securityRole.includes('admin')))) {
-        callback();
-        return router.bleed(403, null, response);
+
+      const userAdmin = check.isUserAdmin(userInfo);
+      if (!userAdmin) {
+        const err = new Error('User role not admin');
+        return bw.redirectWithErrorCode(response, 403, err, callback);
       }
-      db.collection('groups').findOne({ url: groupURL }, { fullname: 1 }, (err, result) => {
-        if (err) {
-          callback();
-          return router.bleed(500, null, response, err);
-        }
-        if (result == null) {
-          callback();
-          return router.bleed(301, '/groups/', response);
-        }
+
+      const groupURL = funcs.getGroupUrlFromUrl(request.url);
+
+      dbMethods.findGroupByUrl(groupURL, db, (err, result) => {
+
+        if (err) return bw.redirectTo500Page(response, err, callback);
+        if (result == null)
+            return bw.redirectTo404Page(response, request.url, callback);
+
         const groupInfo = result;
-        if (request.method == 'POST') {
-          db.collection('groups').deleteOne({ _id: new ObjectID(groupInfo._id) }, err => {
-            if (err) {
-              callback();
-              return router.bleed(500, null, response, err);
-            }
-            console.log(`Group '${groupURL}' deleted!`);
-            db.collection('users').update({ group: new ObjectID(groupInfo._id) }, { $set: { group: ' ' } }, err => {
-              if (err) {
-                console.log(`\n ERROR delete group from users \n error message: ${err}`);
-              }
-              console.log('Group from users is deleted!');
-              callback();
-              return router.bleed(301, '/groups/', response);
-            });
-          });
-        } else {
+
+        if (request.method !== 'POST') {
           return callback({
             title: 'Удаление группы',
             groupInfo
           }, 'groups_delete', 0, 0);
         }
-      });
-    });
+
+        dbMethods.deleteGroup(groupInfo, db, err => {
+
+          if (err) return bw.redirectTo500Page(response, err, callback);
+          console.log(`Group '${groupURL}' deleted!`);
+
+          dbMethods.deleteGroupFromStudentsInfo(groupInfo, db, err => {
+
+            if (err) console.log(`\n ERROR delete group from users info: ${err}`);
+            console.log('Group from users is deleted!');
+            return bw.redirectToGroupsPage(response, callback);
+          });//deleteGroupFromStudentsInfo
+        });//deleteGroup
+      });//findGroupByUrl
+    });//getRoleForAuthedUser
   }
-}
+};
