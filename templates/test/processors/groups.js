0299e6f180846b42e52c048be2310317861b788e
Alex Podshivalov <ksndr.pdsh@gmail.com>
2021-06-19 09:44:49 +0300

    Rewrite with using group dbMethods

--- templates/test/processors/groups.js
+++ templates/test/processors/groups.js
@@ -1,47 +1,31 @@
-const router = require('../../../router');
+const bw = require('./common/bleed_wrapper.js'),
+  check = require('./common/permission_check.js'),
+  dbMethods = require('./groups/dbMethods.js');
 
 module.exports = {
   path: new RegExp('^/groups/$'),
   processor(request, response, callback, sessionContext, sessionToken, db) {
-    if (sessionToken == null || sessionContext == undefined || sessionContext == null) {
-      callback();
-      return router.bleed(301, '/login/', response);
-    }
-    db.collection('users').findOne({ _id: sessionContext.id }, { username: 1, securityRole: 1 }, (err, result) => {
-      if (err) {
-        callback();
-        return router.bleed(500, null, response, err);
-      }
+
+    const userAuthed = check.isUserAuthed(sessionContext, sessionToken);
+    if (!userAuthed) return bw.redirectToLoginPage(response, callback);
+
+    dbMethods.getRoleForAuthedUser(sessionContext.id, db, (err, result) => {
+
+      if (err) return bw.redirectTo500Page(response, err, callback);
       const userInfo = result;
-      db.collection('groups').aggregate([
-        {
-          $lookup:
-           {
-             from: 'users',
-             let: { elder: '$elder' },
-             pipeline: [
-               { $match:
-                 { $expr:
-                   { $eq: ['$_id', '$$elder'] }
-                 }
-               },
-               { $project: { lastName: 1, name: 1 } },
-             ],
-             as: 'elderInfo'
-           }
-        }
-      ]).sort({ course: -1 }).toArray((err, result) => {
-        if (err) {
-          callback();
-          return router.bleed(500, null, response, err);
-        }
+
+      dbMethods.getGroupListWithElderInfo(db, (err, result) => {
+
+        if (err) return bw.redirectTo500Page(response, err, callback);
         const groups = result;
+
         return callback({
           title: 'Группы',
           groups,
           userInfo
         }, 'groups', 0, 0);
-      });
-    });
+
+      });//getGroupListWithElderInfo
+    });//getRoleForAuthedUser
   }
-}
+};
